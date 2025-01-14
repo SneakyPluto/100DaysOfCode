b17baf27a23ca7fa06ad28a39d1fcdd552a9911f
Travis <sneakyplutosaturn@gmail.com>
2025-01-14 15:44:53 +1000

    Completed dictionary_methods chapter hyperskill

---
+++ hyperskill/exercises/dictionary_methods/the_double.py
@@ -0,0 +1,12 @@
+letters = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+}
+
+# letters_dict = dict.fromkeys(letters)
+
+# Initializing with a value
+# value = letters.dict
+
+
+double_alphabet = {key: key * 2 for key in letters}
+print(double_alphabet)
