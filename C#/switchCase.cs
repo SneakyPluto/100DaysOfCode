1a9b3318b97169d8bc7906f293bedf88ed2c3099
Travis <sneakyplutosaturn@gmail.com>
2025-03-09 21:13:23 +1000

    Revise C# switch statement

---
+++ C#/switchCase.cs
@@ -0,0 +1,24 @@
+namespace BroFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String day_of_week = "Tuesday";
+
+            switch (day_of_week = "Tuesday") 
+            {
+                case "Monday":
+                    console.WriteLine("Fav day of week");
+                break;
+                case "Tuesday":
+                    console.WriteLine("Yawn");
+                break;
+                case "Wednesday":
+                    console.WriteLine("Wednesday my due");
+                break;
+            }
+)
+        }
+    }
+}
